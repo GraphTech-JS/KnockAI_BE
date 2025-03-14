cafba49291dc1b50dc79501d3599b64a98247f0e
idimon4uk <d.matskevych@geniusee.com>
2025-03-14 21:01:43 +0200

    fix

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /app
 
 COPY package.json ./
 COPY package-lock.json ./
+COPY index.js ./
 
 RUN npm install --production
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import authRouter from "./routers/auth.js";
 
+const PORT = process.env.PORT || 3000;
 function loging(req, res, next) {
   console.log("Request received at:  ", req.url);
   next();
@@ -16,6 +17,6 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
